64f10099de0f7225b3df565fe81c429fbdd993ec
shaikh1994 <shaikh95@live.com>
2023-01-04 03:27:49 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,21 +1,21 @@
-# import logging
+import logging
 
-# import azure.functions as func
-# from flask import Flask, request, redirect
+import azure.functions as func
+from flask import Flask, request, redirect
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-#     """Each request is redirected to the WSGI handler.
-#     """
-#     return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    """Each request is redirected to the WSGI handler.
+    """
+    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
 
-# @app.route("/")
-# def index():
-#     logging.info('Python HTTP trigger function processed a request.')
+@app.route("/")
+def index():
+    logging.info('Python HTTP trigger function processed a request.')
    
-#     return redirect("https://github.com/Azure-Samples/flask-app-on-azure-functions/", code=302)
-#     #return func.HttpResponse(vscode_redirect, status_code=302)
+    return redirect("https://github.com/Azure-Samples/flask-app-on-azure-functions/", code=302)
+    #return func.HttpResponse(vscode_redirect, status_code=302)
 
 
 
