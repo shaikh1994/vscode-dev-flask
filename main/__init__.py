614983820fde03cb51c43921237acfc2191ae19a
shaikh1994 <shaikh95@live.com>
2023-01-04 02:45:47 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,17 +13,8 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
 @app.route("/")
 def index():
     logging.info('Python HTTP trigger function processed a request.')
-    
-    referer = request.headers.get("Referer", "")
-    logging.info(f"got a referer: {referer}")
-
-    if referer.startswith("https://github.com"):
-        url_part = referer.split("https://github.com/")[-1]
-        vscode_redirect = f"https://vscode.dev/github/{url_part}"
-    else:
-        vscode_redirect = "https://vscode.dev/"
-
-    return redirect(vscode_redirect, code=302)
+   
+    return redirect("https://github.com/Azure-Samples/flask-app-on-azure-functions/", code=302)
     #return func.HttpResponse(vscode_redirect, status_code=302)
 
 
