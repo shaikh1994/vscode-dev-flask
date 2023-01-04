d4253331a2a47f377e1cbd58a05fd03a4630dd7a
shaikh1994 <shaikh95@live.com>
2023-01-04 21:46:06 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,50 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
+
+# import slack
+# import os
+# from pathlib import Path
+# from dotenv import load_dotenv
+# from flask import Flask
+# from slackeventsapi import SlackEventAdapter
+
+# env_path= Path('.')/'.env'
+# load_dotenv(dotenv_path=env_path)
+
+# app = Flask(__name__)
+# slack_event_adapter = SlackEventAdapter(
+#     os.environ['SIGNING_SECRET'], '/slack/events', app)
+
+# client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+# BOT_ID = client.api_call("auth.test")['user_id']
+
+# @slack_event_adapter.on('message')
+# def message(payload):
+#     event = payload.get('event', {})
+#     channel_id = event.get('channel')
+#     user_id = event.get('user')
+#     text = event.get('text')
+
+#     if BOT_ID != user_id:
+#         client.chat_postMessage(channel=channel_id, text=text)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+
+      
 # import logging
 
 # import azure.functions as func
@@ -16,39 +63,6 @@
    
 #     return redirect("https://github.com/Azure-Samples/flask-app-on-azure-functions/", code=302)
 #     #return func.HttpResponse(vscode_redirect, status_code=302)
-
-
-import slack
-import os
-from pathlib import Path
-from dotenv import load_dotenv
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
-
-env_path= Path('.')/'.env'
-load_dotenv(dotenv_path=env_path)
-
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'], '/slack/events', app)
-
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-BOT_ID = client.api_call("auth.test")['user_id']
-
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-
-    if BOT_ID != user_id:
-        client.chat_postMessage(channel=channel_id, text=text)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
 
 
 
